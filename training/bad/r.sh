0e4f11d258f35e68816f18b1219d2b63f6650d52
= <luke.metz@students.olin.edu>
2013-04-16 22:54:13 +0000

    more training, had to over weight blank images a lot

---
+++ training/bad/r.sh
@@ -0,0 +1,23 @@
+convert -rotate 90 21.png 21a.png
+convert -rotate 180 21.png 21b.png
+convert -rotate 270 21.png 21c.png
+
+convert -rotate 90 22.png 22a.png
+convert -rotate 180 22.png 22b.png
+convert -rotate 270 22.png 22c.png
+
+convert -rotate 90 23.png 23a.png
+convert -rotate 180 23.png 23b.png
+convert -rotate 270 23.png 23c.png
+
+convert -rotate 90 24.png 24a.png
+convert -rotate 180 24.png 24b.png
+convert -rotate 270 24.png 24c.png
+
+convert -rotate 90 25.png 25a.png
+convert -rotate 180 25.png 25b.png
+convert -rotate 270 25.png 25c.png
+
+convert -rotate 90 26.png 26a.png
+convert -rotate 180 26.png 26b.png
+convert -rotate 270 26.png 26c.png
---
+++ training/bad/r2.sh
@@ -0,0 +1,113 @@
+convert -rotate 90 27.png 27a.png
+convert -rotate 180 27.png 27b.png
+convert -rotate 270 27.png 27c.png
+
+
+convert -rotate 90 28.png 28a.png
+convert -rotate 180 28.png 28b.png
+convert -rotate 270 28.png 28c.png
+
+
+convert -rotate 90 29.png 29a.png
+convert -rotate 180 29.png 29b.png
+convert -rotate 270 29.png 29c.png
+
+
+convert -rotate 90 30.png 30a.png
+convert -rotate 180 30.png 30b.png
+convert -rotate 270 30.png 30c.png
+
+
+convert -rotate 90 31.png 31a.png
+convert -rotate 180 31.png 31b.png
+convert -rotate 270 31.png 31c.png
+
+
+convert -rotate 90 32.png 32a.png
+convert -rotate 180 32.png 32b.png
+convert -rotate 270 32.png 32c.png
+
+
+convert -rotate 90 33.png 33a.png
+convert -rotate 180 33.png 33b.png
+convert -rotate 270 33.png 33c.png
+
+
+convert -rotate 90 34.png 34a.png
+convert -rotate 180 34.png 34b.png
+convert -rotate 270 34.png 34c.png
+
+
+convert -rotate 90 35.png 35a.png
+convert -rotate 180 35.png 35b.png
+convert -rotate 270 35.png 35c.png
+
+
+convert -rotate 90 36.png 36a.png
+convert -rotate 180 36.png 36b.png
+convert -rotate 270 36.png 36c.png
+
+
+convert -rotate 90 37.png 37a.png
+convert -rotate 180 37.png 37b.png
+convert -rotate 270 37.png 37c.png
+
+
+convert -rotate 90 38.png 38a.png
+convert -rotate 180 38.png 38b.png
+convert -rotate 270 38.png 38c.png
+
+
+convert -rotate 90 39.png 39a.png
+convert -rotate 180 39.png 39b.png
+convert -rotate 270 39.png 39c.png
+
+
+convert -rotate 90 40.png 40a.png
+convert -rotate 180 40.png 40b.png
+convert -rotate 270 40.png 40c.png
+
+
+convert -rotate 90 41.png 41a.png
+convert -rotate 180 41.png 41b.png
+convert -rotate 270 41.png 41c.png
+
+
+convert -rotate 90 42.png 42a.png
+convert -rotate 180 42.png 42b.png
+convert -rotate 270 42.png 42c.png
+
+
+convert -rotate 90 43.png 43a.png
+convert -rotate 180 43.png 43b.png
+convert -rotate 270 43.png 43c.png
+
+
+convert -rotate 90 44.png 44a.png
+convert -rotate 180 44.png 44b.png
+convert -rotate 270 44.png 44c.png
+
+
+convert -rotate 90 45.png 45a.png
+convert -rotate 180 45.png 45b.png
+convert -rotate 270 45.png 45c.png
+
+
+convert -rotate 90 46.png 46a.png
+convert -rotate 180 46.png 46b.png
+convert -rotate 270 46.png 46c.png
+
+
+convert -rotate 90 47.png 47a.png
+convert -rotate 180 47.png 47b.png
+convert -rotate 270 47.png 47c.png
+
+
+convert -rotate 90 48.png 48a.png
+convert -rotate 180 48.png 48b.png
+convert -rotate 270 48.png 48c.png
+
+
+convert -rotate 90 49.png 49a.png
+convert -rotate 180 49.png 49b.png
+convert -rotate 270 49.png 49c.png
