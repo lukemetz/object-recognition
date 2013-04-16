45582e5a290671320af9972fac3279ba1c4b2189
Luke Metz <luke.metz@students.olin.edu>
2013-04-16 12:19:32 +0000

    added glob from interwebs

---
+++ glob.hpp
@@ -0,0 +1,18 @@
+//Thanks
+//http://stackoverflow.com/questions/8401777/simple-glob-in-c-on-unix-system
+
+#include <glob.h>
+#include <vector>
+#include <string>
+
+inline std::vector<std::string> glob(const std::string& pat){
+    using namespace std;
+    glob_t glob_result;
+    glob(pat.c_str(),GLOB_TILDE,NULL,&glob_result);
+    vector<string> ret;
+    for(unsigned int i=0;i<glob_result.gl_pathc;++i){
+        ret.push_back(string(glob_result.gl_pathv[i]));
+    }
+    globfree(&glob_result);
+    return ret;
+}
